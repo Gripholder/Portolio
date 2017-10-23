57aa2d0ed9f8f6728d505bed2f325289583b43eb
Mohamed Elkarim <mohdawad2010@hotmail.com>
2017-10-23 17:23:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log("Hey");
+//
+// $(".hire").mouseover(function() {
+//   $(".hire").css("background-color", "rgba(255,255,255,1)");
+//   $(".hire").css("color", "rgba(0,0,0,1)");
+//   $(".hire").css("width", "700px");
+// });
+//
+// $(".hire").mouseout(function() {
+//   $(".hire").css("background-color", "rgba(255,255,255,0)");
+//   $(".hire").css("color", "rgba(255,255,255,1)");
+//   $(".hire").css("width", "500px");
+// });
