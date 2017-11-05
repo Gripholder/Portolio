4b64297a5fb4d1cabf97eaf5555867bbdfcb7f74
Mohamed Elkarim <mohdawad2010@hotmail.com>
2017-11-05 15:53:10 -0500

    added styling to the portfolio

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// console.log("Hey");
-//
-// $(".hire").mouseover(function() {
-//   $(".hire").css("background-color", "rgba(255,255,255,1)");
-//   $(".hire").css("color", "rgba(0,0,0,1)");
-//   $(".hire").css("width", "700px");
-// });
-//
-// $(".hire").mouseout(function() {
-//   $(".hire").css("background-color", "rgba(255,255,255,0)");
-//   $(".hire").css("color", "rgba(255,255,255,1)");
-//   $(".hire").css("width", "500px");
-// });
+console.log("connected")
+
+$(".hire").click(function() {
+  var offset = 20;
+
+$('html, body').animate({
+    scrollTop: $("#fifthPage").offset().top + offset
+}, 2000);
+})
+
+$(".getStarted").click(function() {
+  $(".form").append("<p>hello</p>")
+})
