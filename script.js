1e515232e3834040dd9019c608c25bb2949291aa
Mohamed Elkarim <mohdawad2010@hotmail.com>
2017-12-19 02:41:51 -0500

    modified resume button

--- script.js
+++ script.js
@@ -1,113 +1,19 @@
-console.log("connected")
+console.log("connected");
 
 $(".hire").click(function() {
   var offset = 20;
 
-$('html, body').animate({
-    scrollTop: $("#fifthPage").offset().top + offset
-}, 2000);
-})
+  $("html, body").animate(
+    {
+      scrollTop: $("#fifthPage").offset().top + offset
+    },
+    2000
+  );
+});
 
 $(".getStarted").click(function() {
-// $("body").css({
-//   "background-color": "black",
-//   "font-size": "200%"
-// })
+  let form = $(".form");
 
-  let form = $(".form")
-
-
-  form.css({"visibility": "visible"})
-  form.animate({height: "100%", width: "100%"})
-
-  // $(this).animate({height: "100%", width: "100%"})
-
-  // $(this).html("<div id='form'></div>")
-
-  // $(this).children("div").animate({height: "100%", width: "100%"})
-
-
-  // $(this).children("div").css("background-color: black;")
-
-  // let div = $(this).children("div")
-
-  // div.html("<h1 id=formHead>Hello World</h1>")
-  // $("#fifthPage").append("<span id='form'></span>")
-  // console.log("Appended")
-
-  // $(".form").append("<p>hello</p>")
-})
-
-// Testing the form animation
-
-// $(function() {
-//       //the form wrapper (includes all forms)
-//     var $form_wrapper	= $('#form_wrapper'),
-//       //the current form is the one with class active
-//       $currentForm	= $form_wrapper.children('form.active'),
-//       //the change form links
-//       $linkform		= $form_wrapper.find('.linkform');
-//
-//     //get width and height of each form and store them for later
-//     $form_wrapper.children('form').each(function(i){
-//       var $theForm	= $(this);
-//       //solve the inline display none problem when using fadeIn fadeOut
-//       if(!$theForm.hasClass('active'))
-//         $theForm.hide();
-//       $theForm.data({
-//         width	: $theForm.width(),
-//         height	: $theForm.height()
-//       });
-//     });
-//
-//     //set width and height of wrapper (same of current form)
-//     setWrapperWidth();
-//
-//     /*
-//     clicking a link (change form event) in the form
-//     makes the current form hide.
-//     The wrapper animates its width and height to the
-//     width and height of the new current form.
-//     After the animation, the new form is shown
-//     */
-//     $linkform.bind('click',function(e){
-//       var $link	= $(this);
-//       var target	= $link.attr('rel');
-//       $currentForm.fadeOut(400,function(){
-//         //remove class active from current form
-//         $currentForm.removeClass('active');
-//         //new current form
-//         $currentForm= $form_wrapper.children('form.'+target);
-//         //animate the wrapper
-//         $form_wrapper.stop()
-//                .animate({
-//                 width	: $currentForm.data('width') + 'px',
-//                 height	: $currentForm.data('height') + 'px'
-//                },500,function(){
-//                 //new form gets class active
-//                 $currentForm.addClass('active');
-//                 //show the new form
-//                 $currentForm.fadeIn(400);
-//                });
-//       });
-//       e.preventDefault();
-//     });
-//
-//     function setWrapperWidth(){
-//       $form_wrapper.css({
-//         width	: $currentForm.data('width') + 'px',
-//         height	: $currentForm.data('height') + 'px'
-//       });
-//     }
-//
-//     /*
-//     for the demo we disabled the submit buttons
-//     if you submit the form, you need to check the
-//     which form was submited, and give the class active
-//     to the form you want to show
-//     */
-//     $form_wrapper.find('input[type="submit"]')
-//            .click(function(e){
-//             e.preventDefault();
-//            });
-//   });
+  form.css({ visibility: "visible" });
+  form.animate({ height: "100%", width: "100%" });
+});
