0e673d7e292af8b6fd25288c55fe876e9dd02ecf
Mohamed Elkarim <mohdawad2010@hotmail.com>
2017-12-20 17:33:11 -0500

    added arrow animation

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
-console.log("connected");
 
-$(".hire").click(function() {
+// $(document).ready(arrowAnimate)
+
+$(".hire").click(scrollToFifthPage)
+$(".learn").click(scrollToSecondPage)
+
+console.log($("#arrow").css("width"))
+
+var arrow = $("#arrow")
+var pos = 0
+
+var arrowDown = setInterval(arrowDownFunction, 20)
+// var arrowUp
+
+function arrowDownFunction() {
+  if(pos < 100){
+    arrow.css("top", pos + "px")
+    pos++
+  } else {
+    clearInterval(arrowDown)
+    arrowUp = setInterval(arrowUpFunction, 20)
+  }
+}
+
+var arrowUp = setInterval(arrowUp, 20)
+
+function arrowUpFunction() {
+  if(0 <= pos && pos <= 100) {
+    arrow.css("top", pos + "px")
+    pos--
+  } else {
+    clearInterval(arrowUp)
+    arrowDown = setInterval(arrowDownFunction, 20)
+  }
+}
+
+// setInterval(function () {
+//   if (pos < 50){
+//       arrow.css("top", pos + "px")
+//       console.log(arrow.css("top"))
+//       pos++
+//       pos1--
+//   } elseif (pos1 <= 50) {
+//     arrow.css("top", pos + "px")
+//     console.log(arrow.css("top"))
+//     pos1++
+//   }
+//   // console.log(arrow)
+//   // arrow = arrow + 1
+//   console.log(arrow)
+// }, 10)
+
+function scrollToFifthPage() {
   var offset = 20;
 
   $("html, body").animate(
@@ -9,11 +59,15 @@ $(".hire").click(function() {
     },
     2000
   );
-});
+};
 
-$(".getStarted").click(function() {
-  let form = $(".form");
+function scrollToSecondPage() {
+  var offset = 20;
 
-  form.css({ visibility: "visible" });
-  form.animate({ height: "100%", width: "100%" });
-});
+  $("html, body").animate(
+    {
+      scrollTop: $("#secondPage").offset().top + offset
+    },
+    1000
+  );
+}
